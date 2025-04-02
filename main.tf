b3e3dc853aa962058a5e5b740f971c0121bf053b
kytan2992 <kytan2992@gmail.com>
2025-04-02 09:38:52 +0800

    update new s3 bucket

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
